8940e18f1401274725f32c3b5e2ac03d321fa39c
Vincent <cloudy.sonata@gmail.com>
2017-11-16 03:24:55 -0500

    Fragment navigation implemented

--- app/src/main/java/frostlight/FrostlightCarRadar/MainActivity.java
+++ app/src/main/java/frostlight/FrostlightCarRadar/MainActivity.java
@@ -1,33 +1,47 @@
 package frostlight.FrostlightCarRadar;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
+public class MainActivity extends AppCompatActivity implements RadarFragment.OnFragmentInteractionListener,
+        MapFragment.OnFragmentInteractionListener {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // TODO: Implement Navigation
+            Fragment fragment = null;
+            Class fragmentClass = null;
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                case R.id.navigation_radar:
+                    fragmentClass = RadarFragment.class;
+                    break;
+                case R.id.navigation_map:
+                    fragmentClass = MapFragment.class;
+                    break;
+                default:
+                    return false;
             }
-            return false;
+
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            // Replace the fragment with the new fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.RadarFragment, fragment).commit();
+            return true;
         }
     };
 
@@ -36,9 +50,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        // Set up bottom navigation
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
